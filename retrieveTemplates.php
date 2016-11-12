1995959e36b18a008837061e90b371d5e531499a
Oli <oliharriswatts@gmail.com>
2016-11-12 16:22:28 +0000

    importing templates

--- retrieveTemplates.php
+++ retrieveTemplates.php
@@ -1,7 +1,7 @@
 <?php
 	$con = mysqli_connect("localhost", "root", "root")or die(mysql_error());
 	mysqli_select_db($con ,"project")or die("Cannot connect to database");
-	$query = mysqli_query($con, "Select * from templates WHERE `Template Name` = 'Test without CSS/JS'");
+	$query = mysqli_query($con, "Select * from templates WHERE `Template Name` = 'Test with CSS'");
 	if (mysqli_num_rows($query)==1) {
 		$row = mysqli_fetch_assoc($query);
 
--- webpageEditor.php
+++ webpageEditor.php
@@ -14,25 +14,52 @@
         dataType: "json"
 	    }).done(function(data){
 	        var html = JSON.stringify(data);
-	        html = html.replace(/(?:\\[rnt]|[\r\n\t]+)+/g, "");
 	        console.log(html);
-	        $(".htmlContainer").append(html);
+	        html = html.replace(/( ?:\\[rnt]|[\r\n\t]+)+/g, "");
+	        $(".htmlContainer").append(getStyles(html));
+	        var sections = splitSections(html);
+	        $(".htmlContainer").append(sections);
+	        document.getElementById('container').contentWindow.document.write(getStyles(html));
+	        document.getElementById('container').contentWindow.document.write(sections[0].outerHTML);
+	        document.getElementById('container').contentWindow.document.write(sections[1].outerHTML);
+	        document.getElementById('container').contentWindow.document.write(sections[2].outerHTML);
 	    });
 
 	    function splitSections (html) {
+	    	html = html.substring(1,html.length-1);
 	    	var elements = $(html);
 	    	var sections = [];
-
 	    	for(i=0;i<elements.length;i++) {
 	    		if(elements[i].tagName == "SECTION") {
 	    			sections.push(elements[i]);
 	    		}
 	    	}
+	    	return sections;
+	    }
+
+	    function getStyles (html) {
+	    	html = html.substring(1,html.length-1);
+	    	var elements = $(html);
+	    	for(i=0;i<elements.length;i++) {
+	    		if(elements[i].tagName == "STYLE") {
+	    			return "<style type='text/css'>" + elements[i].innerHTML + "</style>";
+	    		}
+	    	}
 	    }
 	});
 </script>
+<style type="text/css">
+	.htmlContainer section {
+		border: dashed; 1px;
+		border-color: black;
+		margin: 5px;
+	}
+</style>
 </head>
 <body>
+<section><h1>TESTET</h1></section>
 <div class="htmlContainer"></div>
+<section><h1>TESTET</h1></section>
+<iframe id="container"></iframe>
 </body>
 </html> 
